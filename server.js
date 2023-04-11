78a92e8d58cbcf8da9735730efe4ded4715add27
donglq <donglq2@gmail.com>
2023-04-11 23:50:07 +0700

    update

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const favoriteFood = process.env.FAVORITE_FOOD;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log(`My favorite food is ${favoriteFood}`);
     await sleep(5000);
   }
 }
